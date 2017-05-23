0888b1bb9c1a4bb03306458cd1f015bf0a3e9cae
Paritosh Gupta <paritoshgpt1@yahoo.co.uk>
2017-05-23 11:57:27 +0530

    completed the app

--- Timers/ViewController.swift
+++ Timers/ViewController.swift
@@ -10,14 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timerLabel: UILabel!
+    var timer = Timer()
+    var counter = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        counter = 10
+        timerLabel.text = String(counter)
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.timerFunction), userInfo: nil, repeats: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func timerFunction() {
+        // our function for timer
+        timerLabel.text = String(counter)
+        counter -= 1
+        
+        if counter == 0 {
+            timer.invalidate()
+            timerLabel.text = "Time is up!"
+        }
     }
 
 
